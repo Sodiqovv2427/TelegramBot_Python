43016c1d20bbbe28cc192f1d1aeba2c94c4150f1
Sodiqov2427 <alishersodiqov1188@gmail.com>
2026-09-16 09:49:09 +0500

    ALI-8

--- cache.py
+++ cache.py
@@ -18,7 +18,7 @@ import json
 import logging
 from typing import Any, Optional
 
-import redis.asyncio as redis
+import redis.asynci as redis
 from aiogram.fsm.storage.redis import RedisStorage
 
 from config import config
